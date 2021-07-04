62d466c95b50a246f5c08e54cf7ec248bf2ed0db
Abhinav Mukherjee <aaricrubel@gmail.com>
2021-07-04 23:22:09 +0530

    Added Color Picker

--- script.js
+++ script.js
@@ -70,9 +70,48 @@ penIcon.addEventListener('click', () => {
   penContainer.classList.toggle('display-pen-container');
 });
 
-const pickAColor = document.getElementById('pickAColor');
-console.log(pickAColor);
-pickAColor.addEventListener('change', (e) => {
-  console.log(e.target.value);
-  pen.color = e.target.value;
+const colorPickerBtn = document.getElementById('colorPickerBtn');
+
+const pickr = Pickr.create({
+  el: '.color-picker',
+  theme: 'classic',
+
+  swatches: [
+    'rgba(244, 67, 54, 1)',
+    'rgba(233, 30, 99, 0.95)',
+    'rgba(156, 39, 176, 0.9)',
+    'rgba(103, 58, 183, 0.85)',
+    'rgba(63, 81, 181, 0.8)',
+    'rgba(33, 150, 243, 0.75)',
+    'rgba(3, 169, 244, 0.7)',
+    'rgba(0, 188, 212, 0.7)',
+    'rgba(0, 150, 136, 0.75)',
+    'rgba(76, 175, 80, 0.8)',
+    'rgba(139, 195, 74, 0.85)',
+    'rgba(205, 220, 57, 0.9)',
+    'rgba(255, 235, 59, 0.95)',
+    'rgba(255, 193, 7, 1)',
+  ],
+
+  components: {
+    preview: true,
+    opacity: true,
+    hue: true,
+
+    interaction: {
+      hex: true,
+      rgba: true,
+      hsla: true,
+      hsva: true,
+      cmyk: true,
+      input: true,
+      clear: true,
+      save: true,
+    },
+  },
+});
+
+pickr.on('change', (...args) => {
+  let color = args[0].toRGBA();
+  pen.color = `rgba(${color[0]},${color[1]},${color[2]},${color[3]})`;
 });
